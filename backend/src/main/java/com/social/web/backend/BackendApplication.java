03ee5643968af35989dcf8677fbcca71191cd603
Manasminsk <manasminsk@gmail.com>
2025-05-22 23:42:26 +0530

    Update components

--- backend/src/main/java/com/social/web/backend/BackendApplication.java
+++ backend/src/main/java/com/social/web/backend/BackendApplication.java
@@ -2,12 +2,16 @@ package com.social.web.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.social"         // existing package
+})
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
