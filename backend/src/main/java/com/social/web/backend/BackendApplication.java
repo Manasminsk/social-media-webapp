022bb63353ff55a3d421602ce1815815b16af0de
Manasminsk <manasminsk@gmail.com>
2025-05-23 00:04:13 +0530

    Fix repo scans

--- backend/src/main/java/com/social/web/backend/BackendApplication.java
+++ backend/src/main/java/com/social/web/backend/BackendApplication.java
@@ -3,16 +3,16 @@ package com.social.web.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
-    "com.social" ,  
-	 "com.social.repositories"      // existing package
+    "com.social", // include all your controllers and services
 })
+@EnableJpaRepositories(basePackages = "com.social.repositories") // 👈 tells Spring where to find JPA repos
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
